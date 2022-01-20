9f3366cd73b8498223cbcce7176722ee1f6bf969
Edgar Khudoyan <khudoyanedgar@gmail.com>
2022-01-20 21:23:06 +0400

    call fakeBind

--- 02_fakeBind.js
+++ 02_fakeBind.js
@@ -4,5 +4,13 @@ const user = {
 };
 
 function foo(a, b) {
-  return `name: ${this.name}, a: ${a}, b: {b}`;
+  return `name: ${this.name}, a: ${a}, b: ${b}`;
 }
+
+function fakeBind(foo, context, ...rest) {
+  return function (...args) {
+    return foo.call(context, ...rest.concat(args));
+  };
+}
+
+console.log(fakeBind(foo, user, '5', 'minutes'));
